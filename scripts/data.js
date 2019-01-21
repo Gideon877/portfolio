f6cc37dee47b5067ab278ef4fc57d4bb0f51df3a
gideon877 <thabang@getwala.com>
2019-01-21 23:51:14 +0200

    wip

--- scripts/data.js
+++ scripts/data.js
@@ -21,16 +21,16 @@ const Footer = [
         header: 'Platforms',
         column: 'four',
         link: [
+            // {
+            //     url: 'https://www.facebook.com/gideon877',
+            //     title: 'Facebook'
+            // },
+            // {
+            //     url: '#',
+            //     title: 'Instagram'
+            // },
             {
-                url: 'https://www.facebook.com/xxtramilemsic',
-                title: 'Facebook'
-            },
-            {
-                url: '#',
-                title: 'Instagram'
-            },
-            {
-                url: 'https://twitter.com/xxtramile',
+                url: 'https://twitter.com/gideon877',
                 title: 'Twitter'
             }
         ]
@@ -41,7 +41,7 @@ const Footer = [
         column: 'four',
         link: [
             {
-                url: '#',
+                url: '',
                 title: `<i class="flag icon"></i>February 2018 ©`
             }
         ]
@@ -60,8 +60,8 @@ const myWorkHistory = [
         company: "Wala",
         color: "teal",
         description: {
-            1: " For the past year as a Full Stack Software Engineer at Wala Digital Finance, I've been a part of a small team building out the Wala financial platform. This is a platform that utilizes the blockchain technology to allow users who don't have access to traditional banking institutions to earn, save and spend their money in the form of the Dala cryptocurrency on the platform. This also allowed them to send money from their wallets to their peers across borders at zero fees.",
-            2: " My contribution and responsibilities to the development of this platform were across the board in terms of maintaining the current code base as well as fixing bugs and building out new features as required.",
+            1: "For the past year as a Full Stack Software Engineer at Wala Digital Finance, I've been a part of a small team building out the Wala financial platform. This is a platform that utilizes the blockchain technology to allow users who don't have access to traditional banking institutions to earn, save and spend their money in the form of the Dala cryptocurrency on the platform. This also allowed them to send money from their wallets to their peers across borders at zero fees.",
+            2: "My contribution and responsibilities to the development of this platform were across the board in terms of maintaining the current code base as well as fixing bugs and building out new features as required.",
             3: "We work in an agile environment which requires us to move between frontend and backend aspects as the priorities dictates.",
             4: "This allowed me to work with a range of technologies such as Amazon DynamoDB - NoSQL Database, AWS Step Functions - Flow management between microservices to build business processes, React-Native, Redux, Redux Saga, ImmutableJS.",
             5: " I also had ownership over the rewards component of the platform. implementing new reward rules using JSON Rule Engine, calculating rewards fee for each user."
