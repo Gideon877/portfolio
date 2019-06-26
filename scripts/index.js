1a8ffe4faa6bfea3d8a2777e9a9af8854c505bd0
Thabang Gideon Magaola <gideon877@live.com>
2019-06-26 13:29:57 +0200

    stash

--- scripts/index.js
+++ scripts/index.js
@@ -6,43 +6,46 @@ $(() => {
     const footer = document.getElementById('footer');
     const footerScript = document.getElementById('footerScript');
 
-    const project = document.getElementById('projects');
-    const projectScript = document.getElementById('projectScript');
-    
-    if(workScript) {
-        let workTemplate = Handlebars.compile(workScript.innerHTML);
-        let workTag = workTemplate({
-            data: myWorkHistory
-        });
-        workHistory.innerHTML = workTag;
-    }
-
-    if(footerScript) {
-        let footerTemplate = Handlebars.compile(footerScript.innerHTML);
-        let footerTag = footerTemplate({
-            data: Footer
-        });
-        footer.innerHTML = footerTag;
-    }
-
-    if(projectScript) {
-        let projectTemplate = Handlebars.compile(projectScript.innerHTML);
-        let projectTag = projectTemplate({
-            projects
-        });
-        project.innerHTML = projectTag;
-    }
+    // const project = document.getElementById('projects');
+    // const projectScript = document.getElementById('projectScript');
 
 
+    const projectId = $('#nav-project')[0];
+    const projectScript = $('#projects')[0];
 
-    $('.special.cards .image').dimmer({
-        on: 'hover'
-    });
+    console.log(projectScript);
+
+    let template = Handlebars.compile(projectScript.innerHTML);
+    let display = template({
+        data: {}
+    })
+    projectId.innerHTML = display;
+
+
 
     
-    $('.message .close').on('click', function() {
-        $(this).closest('.message').transition('fade');
-    });
+    // if(workScript) {
+    //     let workTemplate = Handlebars.compile(workScript.innerHTML);
+    //     let workTag = workTemplate({
+    //         data: myWorkHistory
+    //     });
+    //     workHistory.innerHTML = workTag;
+    // }
 
-    $(".ui.accordion").accordion();
+    // if(footerScript) {
+    //     let footerTemplate = Handlebars.compile(footerScript.innerHTML);
+    //     let footerTag = footerTemplate({
+    //         data: Footer
+    //     });
+    //     footer.innerHTML = footerTag;
+    // }
+
+    // if(projectScript) {
+    //     let projectTemplate = Handlebars.compile(projectScript.innerHTML);
+    //     let projectTag = projectTemplate({
+    //         projects
+    //     });
+    //     project.innerHTML = projectTag;
+    // }
+
 });
